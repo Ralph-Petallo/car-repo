c8ff50fed39f2abd0b872880f0a3e4aa9dd859ef
ralphpetallo <ralphpetallo@gmail.com>
2025-09-08 15:26:13 +0800

    new files 2

--- car-rental/routes/web.php
+++ car-rental/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\sampleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('practice');
 });
+
+
+Route::get('request', [sampleController::class,'request'])->name('request');
